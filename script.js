ee30af0a2439fc916d80f1ff7c67191e74aae43b
Alton <asllanialton@gmail.com>
2024-12-25 22:57:56 +0100

    added responsive for header

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 window.addEventListener('scroll', function() {
-    document.querySelector('.header').style.opacity = Math.max(1 - window.scrollY / 300, 0.8);
+  document.querySelector('.header').style.opacity = Math.max(1 - window.scrollY / 300, 0.8);
+});
+
+
+  const toggleMenu = document.getElementById('toggle-menu');
+    const closeMenu = document.getElementById('close-menu');
+    const Nav = document.getElementById('nav');
+
+  toggleMenu.addEventListener('click', () => {
+      Nav.classList.add('active');
   });
-  
- 
+
+  closeMenu.addEventListener('click', () => {
+      Nav.classList.remove('active');
+}); 
