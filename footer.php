6bffc9c92ca41d071481ac6c88af08433a358a6f
Alton <asllanialton@gmail.com>
2025-01-16 15:16:55 +0100

    updated footer

---
+++ footer.php
@@ -0,0 +1,39 @@
+<?php
+    echo 
+    '
+        <footer>
+        <div class="box">
+            <h3>Quick Links</h3>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="menu.html">Menu</a></li>
+                <li><a href="Reservations.html">Reservations</a></li>
+                <li><a href="about.html">About Us</a></li>
+                <li><a href="login.html">Login/Register</a></li>
+            </ul>
+        </div>
+        <div class="box">
+            <h3>Locations</h3>
+            <ul>
+                <li><a href="#">Kosovo</a></li>
+                <li><a href="#">Albania</a></li>
+                <li><a href="#">Germany</a></li>
+            </ul>
+        </div>
+        <div class="box">
+            <h3>Contact Info</h3>
+            <ul>
+                <li><a href="#">043 123 456</a></li>
+                <li><a href="#">aa68723@ubt-uni.net</a></li>
+                <li><a href="#">az68671@ubt-uni.net</a></li>
+                <li><a href="#">Prishtine</a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>© 2025, Të gjitha të drejtat e rezervuara | Made with ❤️ in Prishtinë</p>
+        </div>
+    </footer>
+    '
+?>
+
+
